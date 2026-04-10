0303d9da4109e77906fe80c2d5709c4df6c0e9f9
Sam <taverna.sami@gmail.com>
2026-04-10 10:57:02 +0200

    feat: initialize OneSignal with app ID

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import OneSignal from 'react-onesignal'
 import App from './App'
 import './styles/globals.css'
+
+OneSignal.init({
+  appId: '35189144-5872-41d0-91bb-f3e8197ffb7d',
+  notifyButton: { enable: false },
+  allowLocalhostAsSecureOrigin: true,
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
