532f8dfda6374225de17825a24dfad3831c87556
Sam <taverna.sami@gmail.com>
2026-04-10 11:52:38 +0200

    fix: add OneSignal service worker required for web push

--- src/onesignal.js
+++ src/onesignal.js
@@ -5,4 +5,5 @@ export const initPromise = OneSignal.init({
   appId: '35189144-5872-41d0-91bb-f3e8197ffb7d',
   notifyButton: { enable: false },
   allowLocalhostAsSecureOrigin: true,
+  serviceWorkerParam: { scope: '/' },
 })
